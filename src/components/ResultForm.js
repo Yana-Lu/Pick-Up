8494dd43109d25faf5a0226cbbb5642c09700f37
Yana Lu <a7636439@gmail.com>
2020-12-14 14:13:21 +0800

    try deproy part4

--- src/components/ResultForm.js
+++ src/components/ResultForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { SaveResult } from './Firebase'
 // import { ShowResult } from './Firebase'
